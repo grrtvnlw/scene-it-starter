413397ddaf4c4e5ed181c0c485b0f66a595f8e33
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-28 14:28:23 -0400

    Exercise completed through Step 4

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  function renderMovies(movieArray) {
+    let movieHtmlArray = movieArray.map(function(currentMovie) {
+
+    });
+  }
+});
