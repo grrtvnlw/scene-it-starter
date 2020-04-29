fc2c7fa28a32bfc9823f40aecd4b1b54877cab32
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-29 15:24:55 -0400

    Completed Part 2, Step 3

---
+++ watchlist.js
@@ -0,0 +1,28 @@
+watchlist = localStorage.getItem('watchlist')
+console.log(watchlist)
+
+parsedList = JSON.parse(watchlist)
+console.log(parsedList)
+
+
+function renderMovies(movieArray) {
+  let movieHtmlArray = movieArray.map(currentMovie => {
+    return `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+    <div class="card w-100 h-100 d-flex">
+    <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap" style="height: 65%">
+    <div class="card-body d-flex flex-column justify-content-between align-items-center" style="height: 35%">
+    <h5 class="card-title">${currentMovie.Title}</h5>
+    <p class="card-text">${currentMovie.Year}</p>
+    <button type="button" class="btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')">Remove From Watchlist</button>
+    </div>
+    </div>
+    </div>
+    `
+  });
+  return movieHtmlArray.join('');
+}
+
+const moviesContainer = document.querySelector('#movies-container')
+const moviesRow = document.querySelector('.row')
+moviesContainer.innerHTML = renderMovies(parsedList);
