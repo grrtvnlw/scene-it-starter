4292ee7243db546dfd171cb6ae41d741c74edba2
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-02 19:44:45 -0400

    Updated UI for movie cards

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function saveToWatchlist(imdbID) {
 }
 
 const myForm = document.getElementById('search-form');
-myForm.addEventListener('click', (e) => {
+myForm.addEventListener('submit', (e) => {
   e.preventDefault();
   let searchString = $('.search-bar').val()
   let urlEncodedSearchString = encodeURIComponent(searchString)
@@ -36,17 +36,16 @@ myForm.addEventListener('click', (e) => {
         .then(function (response) {
           moviesContainer.innerHTML += (`
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
-              <div class="card w-100 h-100 d-flex">
-                <img class="card-img-top" src="${response.data.Poster}" onerror="if (this.src != 'no_image.png') this.src='no_image.png';" alt="Card image cap" style="height: 65%">
-                <div class="card-body d-flex flex-column justify-content-between align-items-center w-100 p-0 pt-3 m-0" style="height: 35%">
-                <div class="d-flex flex-column justify-content-evenly align-items-center" style="height: 80%">
-                  <div class="mb-0">
-                    <h5 class="card-title text-center">${response.data.Title}</h5>
-                  </div>  
-                  <div class="dropdown d-flex align-items-start m-0 p-0 mt-2" style="height: 20%">
-                    <p class="card-text mr-4">${response.data.Year}</p>
+              <div class="card w-100 h-100 d-flex flex-column">
+                <div class="d-flex" style="height: 65%">
+                  <img class="card-img-top" src="${response.data.Poster}" onerror="if (this.src != 'no_image.png') this.src='no_image.png';" alt="Card image cap">
+                </div>
+                <div class="card-body d-flex flex-column justify-content-between align-items-center p-0 pt-2 m-0 overflow-auto" style="height: 25%">
+                  <h5 class="card-title text-center m-0 p-0 font-weight-bold">${response.data.Title}</h5>
+                  <div class="dropdown d-flex align-items-center m-0 p-0 pt-2">
+                    <p class="card-text mr-4 m-0 p-0">${response.data.Year}</p>
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-light m-0 p-0" data-toggle="modal" data-target="#${response.data.imdbID}">
+                    <button type="button" class="btn btn-dark m-0 p-0" data-toggle="modal" data-target="#${response.data.imdbID}">
                       Plot
                     </button>
                     <!-- Modal -->
@@ -60,7 +59,7 @@ myForm.addEventListener('click', (e) => {
                             </button>
                           </div>
                           <div class="modal-body">
-                          <label>${response.data.Plot}<label>
+                            <label>${response.data.Plot}<label>
                           </div>
                           <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -69,15 +68,14 @@ myForm.addEventListener('click', (e) => {
                       </div>
                     </div>
                     </div>
-                    <div class="text-center d-flex justify-content-center m-0 p-0 mt-3">
-                      <p class="card-text text-center">IMDB rating: ${response.data.imdbRating}</p>
+                    <div class="text-center d-flex justify-content-center m-0 p-2">
+                      <p class="card-text text-center border border-dark rounded p-1 font-weight-bold"><span class="bg-warning p-1 rounded font-weight-bold">IMDb</span> ${response.data.imdbRating}/10 ⭐️</p>
                     </div>
                   </div>
-                  <div class="d-flex flex-column justify-content-end align-items-center rounded w-100 pb-3" style="height: 20%">
-                    <button type="button" class="btn btn-primary p-1" onclick="saveToWatchlist('${response.data.imdbID}')">Add to Watchlist</button>
+                  <div class="d-flex flex-column justify-content-center align-items-center" style="height: 10%">
+                    <button type="button" class="btn btn-light p-1" onclick="saveToWatchlist('${response.data.imdbID}')">Add to Watchlist</button>
                   </div>
                 </div>
-              </div>
               </div>
             </div>
           `)
