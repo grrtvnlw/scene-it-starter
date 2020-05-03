be9b17d69fd599728af02bb4424f7c294fa57117
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-02 20:36:13 -0400

    Adding jQuery, troubleshooting mobile

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function saveToWatchlist(imdbID) {
   localStorage.setItem('watchlist', watchlistJSON);
 }
 
-const myForm = document.getElementById('search-form');
-myForm.addEventListener('submit', (e) => {
+// const myForm = document.getElementById('search-form');
+$('#search-form').on('click' , function(e) {
   e.preventDefault();
   let searchString = $('.search-bar').val()
   let urlEncodedSearchString = encodeURIComponent(searchString)
@@ -26,8 +26,21 @@ myForm.addEventListener('submit', (e) => {
       renderMovies(movies);
       let movieHTML = renderMovies(movies);
       moviesContainer.innerHTML = movieHTML;
-      return movies  
-    })
+      return movies 
+  })
+
+// myForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   let searchString = $('.search-bar').val()
+//   let urlEncodedSearchString = encodeURIComponent(searchString)
+//   axios.get("http://www.omdbapi.com/?apikey=efe3c50b&s=" + urlEncodedSearchString)
+//     .then(response => {
+//       movies = response.data.Search
+//       renderMovies(movies);
+//       let movieHTML = renderMovies(movies);
+//       moviesContainer.innerHTML = movieHTML;
+//       return movies  
+//     })
 
   function renderMovies(movieArray) {
     let movieHtmlArray = movieArray.map(currentMovie => {
